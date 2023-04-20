c0a4bbaeabd35145eee98904f1e7ce1c6bb22ddd
bin9li <bin9li@foxmail.com>
2023-04-20 11:07:16 +0800

    build v0.1

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,455 @@
+import { defineConfig, DefaultTheme } from 'vitepress'
+
+const ogDescription = 'DataLab | 数据实验室'
+const ogImage = '/images/logo.png'
+const ogTitle = 'DataLab'
+const ogUrl = ''
+
+// netlify envs
+// const deployURL = process.env.DEPLOY_PRIME_URL || ''
+// const commitRef = process.env.COMMIT_REF?.slice(0, 8) || 'dev'
+
+// const deployType = (() => {
+//   switch (deployURL) {
+//     case 'https://main--vite-docs-main.netlify.app':
+//       return 'main'
+//     case '':
+//       return 'local'
+//     default:
+//       return 'release'
+//   }
+// })()
+// const additionalTitle = ((): string => {
+//   switch (deployType) {
+//     case 'main':
+//       return ' (main branch)'
+//     case 'local':
+//       return ' (local)'
+//     case 'release':
+//       return ''
+//   }
+// })()
+// const versionLinks = ((): DefaultTheme.NavItemWithLink[] => {
+//   switch (deployType) {
+//     // case 'main':
+//     case 'local':
+//       return [
+//         {
+//           text: 'Vite 4 Docs (release)',
+//           link: 'https://vitejs.dev',
+//         },
+//         {
+//           text: 'Vite 3 Docs',
+//           link: 'https://v3.vitejs.dev',
+//         },
+//         {
+//           text: 'Vite 2 Docs',
+//           link: 'https://v2.vitejs.dev',
+//         },
+//       ]
+//     case 'release':
+//       return [
+//         {
+//           text: 'Vite 3 Docs',
+//           link: 'https://v3.vitejs.dev',
+//         },
+//         {
+//           text: 'Vite 2 Docs',
+//           link: 'https://v2.vitejs.dev',
+//         },
+//       ]
+//   }
+// })()
+
+export default defineConfig({
+  lang: 'zh-CN',
+  title: 'DataLab | 数据实验室',
+  description: '大数据技术研究与实践记录',
+
+  lastUpdated: true,
+  cleanUrls: true,
+
+  head: [
+    ['link', { rel: 'icon', type: 'image/svg+xml', href: '/images/logo.png' }],
+    ['meta', { property: 'og:type', content: 'website' }],
+    ['meta', { property: 'og:title', content: ogTitle }],
+    ['meta', { property: 'og:image', content: ogImage }],
+    ['meta', { property: 'og:url', content: ogUrl }],
+    ['meta', { property: 'og:description', content: ogDescription }],
+    // ['meta', { name: 'twitter:card', content: 'summary_large_image' }],
+    // ['meta', { name: 'twitter:site', content: '@vite_js' }],
+    ['meta', { name: 'theme-color', content: '#646cff' }],
+    [
+      'script',
+      {
+        src: 'https://cdn.usefathom.com/script.js',
+        'data-site': 'CBDFBSLI',
+        'data-spa': 'auto',
+        defer: '',
+      },
+    ],
+  ],
+
+  locales: {
+    root: { label: '简体中文' },
+    zh: { label: 'English', link: '/en/' },
+  },
+
+  themeConfig: {
+    logo: '/images/logo.png',
+
+    editLink: {
+      pattern: 'https://github.com/bin9li/vite/edit/main/docs/:path',
+      text: '编辑此页',
+    },
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/bin9li' },
+    ],
+
+    // algolia: {
+    //   appId: '7H67QR5P0A',
+    //   apiKey: 'deaab78bcdfe96b599497d25acc6460e',
+    //   indexName: 'vitejs',
+    //   searchParameters: {
+    //     facetFilters: ['tags:en'],
+    //   },
+    // },
+
+    // carbonAds: {
+    //   code: 'CEBIEK3N',
+    //   placement: 'vitejsdev',
+    // },
+
+    footer: {
+      message: 'Released under the MIT License. ',
+      copyright: 'Copyright ©️ 2023-present bin9li & Vite Contributors',
+    },
+
+    nav: [
+      { text: '指引', link: '/main/', activeMatch: '/main/' },
+      { text: '基础知识', link: '/base/', activeMatch: '/base/' },
+      { text: '编程语言', link: '/lang/', activeMatch: '/lang/' },
+      // { text: '数据技术', link: '/data/', activeMatch: '/data/' },
+      { text: '数据技术', 
+        items: [
+          {text: 'HDFS', link: '/data/hdfs/01-Introduction-to-HDFS'} 
+        ],
+        // link: '/data/', activeMatch: '/data/' 
+      },
+      { text: '工具工程', link: '/tool/', activeMatch: '/tool/' },
+      { text: '读书笔记', link: '/book/', activeMatch: '/book/' },
+      {
+        text: '相关链接',
+        items: [
+          { text: '首页', link: '/' },
+          {
+            items: [
+              {
+                text: '百度',
+                link: 'https://www.baidu.com/',
+              },
+              {
+                text: '阿里',
+                link: 'https://ali-home.alibaba.com/',
+              },
+              {
+                text: '腾讯',
+                link: 'https://www.qq.com/',
+              },
+              {
+                text: '字节',
+                link: 'https://www.bytedance.com/',
+              },
+            ],
+          },
+        ],
+      },
+    ],
+
+    sidebar: {
+      '/base/': [
+        {
+          text: '计算机基础',
+          collapsed: false, //折叠子菜单功能
+          items: [
+            {
+              text: '概述',
+              link: '/base/',
+            },
+          ],
+        },
+        // {
+        //   text: '计算机数学',
+        //   collapsed: false, //折叠子菜单功能
+        //   items: [
+        //     {
+        //       text: '概述',
+        //       link: '/base/',
+        //     },
+        //   ],
+        // },
+        // {
+        //   text: '计算机英语',
+        //   collapsed: false, //折叠子菜单功能
+        //   items: [
+        //     {
+        //       text: '概述',
+        //       link: '/base/',
+        //     },
+        //   ],
+        // },
+        {
+          text: '计算机组成原理',
+          collapsed: false, //折叠子菜单功能
+          items: [
+            {
+              text: '概述',
+              link: '/base/',
+            },
+          ],
+        },
+        {
+          text: '计算机网络',
+          collapsed: false, //折叠子菜单功能
+          items: [
+            {
+              text: '概述',
+              link: '/base/',
+            },
+          ],
+        },
+        {
+          text: '数据结构',
+          collapsed: false, //折叠子菜单功能
+          items: [
+            {
+              text: '概述',
+              link: '/base/',
+            },
+          ],
+        },
+        {
+          text: '算法',
+          collapsed: false, //折叠子菜单功能
+          items: [
+            {
+              text: '概述',
+              link: '/base/',
+            },
+          ],
+        },
+        {
+          text: '操作系统',
+          collapsed: false, //折叠子菜单功能
+          items: [
+            {
+              text: '概述',
+              link: '/base/',
+            },
+          ],
+        },
+        {
+          text: '数据库概论',
+          collapsed: false, //折叠子菜单功能
+          items: [
+            {
+              text: '概述',
+              link: '/base/',
+            },
+          ],
+        },
+      ],
+      'lang' :[
+        {
+          text: '编程语言',
+          collapsed: false, //折叠子菜单功能
+          items: [
+            {
+              text: '概述',
+              link: '/lang/',
+            },
+          ],
+        },
+        {
+          text: 'Java',
+          collapsed: false, //折叠子菜单功能
+          items: [
+            {
+              text: 'Java 概述',
+              link: '/lang/',
+            },
+            {
+              text: 'Java 详解',
+              link: '/lang/',
+            },
+          ],
+        },
+        {
+          text: 'Scala',
+          collapsed: false, //折叠子菜单功能
+          items: [
+            {
+              text: 'Scala 概述',
+              link: '/lang/',
+            },
+            {
+              text: 'Scala 详解',
+              link: '/lang/',
+            },
+          ],
+        },
+        {
+          text: 'Python',
+          collapsed: false, //折叠子菜单功能
+          items: [
+            {
+              text: 'Python 概述',
+              link: '/lang/',
+            },
+            {
+              text: 'Python 详解',
+              link: '/lang/',
+            },
+          ],
+        },
+      ],
+      '/data/': [
+        {
+          text: '概述',
+          collapsed: false, //折叠子菜单功能
+          items: [
+            {
+              text: '开始',
+              link: '/data/',
+            },
+          ],
+        },  
+        {
+          text: '数据库技术',
+          collapsed: false, //折叠子菜单功能
+          items: [
+            {
+              text: '开始',
+              link: '/data/',
+            },
+          ],
+        }, 
+        {
+          text: 'HDFS',
+          collapsed: false, //折叠子菜单功能
+          items: [
+            {
+              text: 'HDFS简介',
+              link: '/data/hdfs/01-Introduction-to-HDFS',
+            },
+          ],
+        },   
+      ],
+      '/tool/': [
+        {
+          text: '工具工程技术',
+          collapsed: false, //折叠子菜单功能
+          items: [
+            {
+              text: '概述',
+              link: '/tool/',
+            },
+          ],
+        },  
+        {
+          text: 'Linux',
+          collapsed: false, //折叠子菜单功能
+          items: [
+            {
+              text: '开始',
+              link: '/tool/',
+            },
+          ],
+        }, 
+        {
+          text: 'Docker',
+          collapsed: false, //折叠子菜单功能
+          items: [
+            {
+              text: '开始',
+              link: '/tool/',
+            },
+          ],
+        }, 
+        {
+          text: 'Git',
+          collapsed: false, //折叠子菜单功能
+          items: [
+            {
+              text: '开始',
+              link: '/tool/',
+            },
+          ],
+        }, 
+        {
+          text: '设计模式',
+          collapsed: false, //折叠子菜单功能
+          items: [
+            {
+              text: '开始',
+              link: '/tool/',
+            },
+          ],
+        },  
+        {
+          text: 'Maven',
+          collapsed: false, //折叠子菜单功能
+          items: [
+            {
+              text: '开始',
+              link: '/tool/',
+            },
+          ],
+        },   
+        {
+          text: 'Markdown',
+          collapsed: false, //折叠子菜单功能
+          items: [
+            {
+              text: '开始',
+              link: '/tool/',
+            },
+          ],
+        }, 
+        {
+          text: 'Zookeeper',
+          collapsed: false, //折叠子菜单功能
+          items: [
+            {
+              text: '开始',
+              link: '/tool/',
+            },
+          ],
+        },
+      ], 
+      '/book/': [
+        {
+          text: '读书笔记',
+          collapsed: false, //折叠子菜单功能
+          items: [
+            {
+              text: '概述',
+              link: '/tool/',
+            },
+          ],
+        },  
+        {
+          text: '数据仓库工具箱',
+          collapsed: false, //折叠子菜单功能
+          items: [
+            {
+              text: '开始',
+              link: '/tool/',
+            },
+          ],
+        }, 
+      ], 
+    },
+  },
+})
---
+++ docs/data/index.md
@@ -0,0 +1,93 @@
+# 指引
+
+使用 VitePress v4.2.1 搭建的静态网站。
+
+## 编写
+
+VitePress 带有内置的 Markdown 扩展。
+
+### 头部锚点
+
+头部会自动获取锚点链接。可以通过 `markdown.anchor` 选项配置锚点的渲染。
+
+### 内部链接[#](https://process1024.github.io/vitepress/guide/markdown#%E5%86%85%E9%83%A8%E9%93%BE%E6%8E%A5)
+
+内部链接转换为 SPA 导航的路由链接。 此外，每个子目录中包含的每个 `index.md` 都会自动转换为 `index.html`，并带有相应的URL `/`。
+
+举个例子，现在有以下目录结构：
+
+```
+.
+├─ index.md
+├─ foo
+│  ├─ index.md
+│  ├─ one.md
+│  └─ two.md
+└─ bar
+   ├─ index.md
+   ├─ three.md
+   └─ four.md
+```
+
+在 `foo/one.md`中:
+
+```markdown
+[Home](/) <!-- 点击跳转到 根目录的 index.md -->
+[foo](/foo/) <!-- 点击跳转到 foo 目录的 index.html -->
+[foo heading](./#heading) <!-- 锚点会定位到 foo 的 heading 标题 -->
+[bar - three](../bar/three) <!-- 你可以不写后缀名 -->
+[bar - three](../bar/three.md) <!-- 也可以加 .md -->
+[bar - four](../bar/four.html) <!-- 或者加 .html -->
+```
+
+### 外部链接
+
+外部的链接会自动识别并生成 `target="_blank" rel="noreferrer"` 的链接。
+
+### Emoji
+
+**输入**
+
+```
+:tada: :100:
+```
+
+**输出**
+
+🎉 💯
+
+可用的emoji可以通过[这里](https://github.com/markdown-it/markdown-it-emoji/blob/master/lib/data/full.json)了解
+
+### 生成目录
+
+**输入**
+
+```
+[[toc]]
+```
+
+可以使用 `markdown.toc` 选项配置 TOC 的渲染。
+
+### 容器
+
+::: info
+This is an info box.
+:::
+
+::: tip
+This is a tip.
+:::
+
+::: warning
+This is a warning.
+:::
+
+::: danger
+This is a dangerous warning.
+:::
+
+::: details
+This is a details block.
+:::
+
+可以通过在容器的“类型”后面添加文本来设置自定义标题。
